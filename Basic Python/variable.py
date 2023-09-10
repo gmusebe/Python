5eff3296d3a26f463e58d9c2913852e4a376cc44
Musebe Ivan <musebeivan@gmail.com>
2023-09-10 23:39:39 +0300

    2021-09-10

---
+++ Basic Python/variable.py
@@ -0,0 +1,23 @@
+# Variables
+# Assign x the value of 5
+x = 5
+# x is a container containing the value of 5
+# x is a variable
+
+print(x)
+
+#  Data types
+#  1. Numbers 
+#  Example 2
+y = 6
+z = x + y
+
+print(z)
+
+# Check the class/type of data/variable
+print(type(x))
+
+# Operations on Numbers
+# Arithmetic Operatos () ** * / + -
+b = 3 + 5 * 4 / 16 -1
+print(b)
